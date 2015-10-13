390f7836afba5f3544a25cf6acc9a2ec2ea49c8a
Livanux <ivangastelum.tf@gmail.com>
2015-10-13 12:54:23 -0700

    Fix navbar responsive, click, map

--- js/views.js
+++ js/views.js
@@ -1,20 +1,25 @@
 $(document).ready(function() { 
-  loadIndex();
+	loadIndex();
+	$("#coverabout").hide();
+
 });
 
 function loadAbout(){
 	$("#myCarousel").hide();
+	$("#coverabout").show();
 	$("#content").load("views/about.html");
 	$("#about").css({"color": "#4ca64c","text-shadow": "none !important"});
 }
 
 function loadContact(){
 	$("#myCarousel").hide();
+	$("#coverabout").hide();
 	$("#content").load("views/contact.html");	
 }
 
 function loadIndex(){
 	$("#myCarousel").show();
+	$("#coverabout").hide();
 	$("#content").load("views/index.php");
 }
 
