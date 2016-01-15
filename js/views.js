0706bf1d26bbeafa3aad4de263896e57a6832d3e
m-vega <manuelvega15@hotmail.com>
2016-01-14 19:20:08 -0800

    Several changes

--- js/views.js
+++ js/views.js
@@ -7,31 +7,48 @@ function loadHome(){
 	$("#myCarousel").show();
 	$("#cover").hide();
 	$("#content").load("views/home.php");
+	$(window).scroll(function() {
+               if ($(".navbar").offset().top > 50) {
+                    $(".navbar-fixed-top").addClass("top-nav-collapse");
+                } else{
+                    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+                }
+        });
+    
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
 }
 
 function loadAbout(){
 	$("#myCarousel").hide();
-	$("#cover").show();
 	$("#content").load("views/about.html");
-//	$("#about").css({"color": "#4ca64c","text-shadow": "none !important"});
+	$(window).scroll(function() {$(".navbar-fixed-top").addClass("top-nav-collapse");});
+	//$("#about").css({"color": "#4ca64c","text-shadow": "none !important"});
 }
-
 
 function loadProducts(){
 	$("#myCarousel").hide();
-	$("#cover").show();
-	$("#content").load("#");	
+	$("#content").load("#");
+	$(window).scroll(function() {$(".navbar-fixed-top").addClass("top-nav-collapse");});	
 }
 
 function loadContact(){
 	$("#myCarousel").hide();
-	$("#covercontact").show();
 	$("#content").load("views/contact.html");	
+	$(window).scroll(function() {$(".navbar-fixed-top").addClass("top-nav-collapse");});
 }
 
 function loadGallery(){
 	$("#myCarousel").hide();
-	$("#cover").show();
 	$("#content").load("views/gallery.html");
+	$(window).scroll(function() {$(".navbar-fixed-top").addClass("top-nav-collapse");});
 }
 
--- views/home.php
+++ views/home.php
@@ -2,9 +2,9 @@
 <div class="container home">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header" style="text-align: center !important; font-size: 3em !important;">
-                    <hr>
+                <h1 style="text-align: center !important; font-size: 3em !important;">
                     Welcome to Heirloom Farms
+                    <hr>
                 </h1>
             </div>
             <div class="col-md-4">
